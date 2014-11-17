11479fc5f3aba4102e406c17d69330813e4255fa
slabbu <s236368@stud.hioa.no>
2014-11-17 12:41:46 +0100

    Create Apotek.java

---
+++ Apotek.java
@@ -0,0 +1,41 @@
+/*
+Navn (studentnummer):
+* Nicolai Olsen  (s181082)
+* Vidar Vasrud (s159718)
+* Adrian Moen (s236368)
+Klasse: HINGDATA14HA
+*/
+
+/*< import-setninger >*/
+public class Apotek extends JFrame
+{
+  private Pasientregister pasienter;
+  private JTextField pNavnFelt, pFødtFelt, reseptNrFelt;
+  private JTextArea reseptOmråde;
+  private JButton finnResepter, registrerUtlevert;
+
+  public Apotek( String apotek, Pasientregister p )
+  {
+    super( apotek + " apotek " );
+
+    /*< Oppretter alle komponentene og setter opp brukergrensesnittet. >*/
+
+    /*< Oppretter et lytteobjekt og knytter knappene til lytteobjektet. >*/
+  }
+
+  public void finnResepter()
+  {
+
+  }
+
+  public void registrertUtlevert()
+  {
+    /*< Ut fra pasientens navn, fødselsdato og reseptens nummer skal
+      metoden registerere at medisinen som resepten foreskriver er
+      utlevert. >*/
+  }
+
+  /*< private lytteklasse som fanger opp hedelsene i vinduet. >*/
+
+}// end of class Apotek
+/*a) Programmer metoden public void registrertUtlevert().*/
