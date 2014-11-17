4519a46626a3ef4290c39a0a7877e6cb80f4db9b
slabbu <s236368@stud.hioa.no>
2014-11-17 12:41:17 +0100

    Create Legeregister.java

---
+++ Legeregister.java
@@ -0,0 +1,19 @@
+/*
+Navn (studentnummer):
+* Nicolai Olsen  (s181082)
+* Vidar Vasrud (s159718)
+* Adrian Moen (s236368)
+Klasse: HINGDATA14HA
+*/
+
+public class Legeregister
+{
+  private Lege[] leger;
+
+  public Legeregister( Lege[] l ){ ... }
+
+  public Lege[] getLeger(){ ... }
+
+  < Andre metoder du ikke vil får bruk for i denne oppgaven >
+
+}// end of class Legeregister
